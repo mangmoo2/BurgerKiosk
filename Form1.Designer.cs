20f6d636d2b7f80432dd636e80aea3dd626d4c76
mangmoo2 <jihw0522@gmail.com>
2026-04-09 09:41:36 +0900

    UI 배치 조정 label 2개, GroupBox 3개, RadioButton 3개, CheckBox 4개, ListBox 1개, Button 2개 배치 완

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,237 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            radioButton1 = new RadioButton();
+            groupBox1 = new GroupBox();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            groupBox2 = new GroupBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            groupBox3 = new GroupBox();
+            label2 = new Label();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 24F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.Location = new Point(27, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(298, 45);
+            label1.TabIndex = 0;
+            label1.Text = "버거 주문 키오스크";
+            label1.Click += label1_Click;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            radioButton1.Location = new Point(6, 49);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(76, 25);
+            radioButton1.TabIndex = 1;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "햄버거";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            groupBox1.Location = new Point(27, 94);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(239, 250);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "메뉴 선택";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            radioButton2.Location = new Point(6, 117);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(108, 25);
+            radioButton2.TabIndex = 2;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "불고기버거";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            radioButton3.Location = new Point(6, 187);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(92, 25);
+            radioButton3.TabIndex = 3;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "치킨버거";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(checkBox3);
+            groupBox2.Controls.Add(checkBox4);
+            groupBox2.Controls.Add(checkBox2);
+            groupBox2.Controls.Add(checkBox1);
+            groupBox2.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            groupBox2.Location = new Point(289, 94);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(200, 202);
+            groupBox2.TabIndex = 4;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "추가 옵션";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("맑은 고딕", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            checkBox1.Location = new Point(6, 49);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(79, 21);
+            checkBox1.TabIndex = 5;
+            checkBox1.Text = "감자튀김";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Font = new Font("맑은 고딕", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            checkBox2.Location = new Point(6, 78);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(53, 21);
+            checkBox2.TabIndex = 6;
+            checkBox2.Text = "콜라";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Font = new Font("맑은 고딕", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            checkBox3.Location = new Point(6, 136);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(84, 21);
+            checkBox3.TabIndex = 8;
+            checkBox3.Text = "소스 추가";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Font = new Font("맑은 고딕", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            checkBox4.Location = new Point(6, 107);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(84, 21);
+            checkBox4.TabIndex = 7;
+            checkBox4.Text = "치즈 추가";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(listBox1);
+            groupBox3.Controls.Add(label2);
+            groupBox3.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            groupBox3.Location = new Point(514, 93);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(239, 251);
+            groupBox3.TabIndex = 9;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "주문 내역";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label2.Location = new Point(15, 223);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 20);
+            label2.TabIndex = 0;
+            label2.Text = "총 금액 : 0원";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(15, 32);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(217, 179);
+            listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.Location = new Point(474, 377);
+            button1.Name = "button1";
+            button1.Size = new Size(140, 46);
+            button1.TabIndex = 2;
+            button1.Text = "주문하기";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button2.Location = new Point(633, 377);
+            button2.Name = "button2";
+            button2.Size = new Size(140, 46);
+            button2.TabIndex = 10;
+            button2.Text = "초기화";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private RadioButton radioButton1;
+        private GroupBox groupBox1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private GroupBox groupBox2;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private GroupBox groupBox3;
+        private ListBox listBox1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,30 @@ namespace BurgerKiosk
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
